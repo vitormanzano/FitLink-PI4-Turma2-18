7ae6e24ef745864deb3531311c0f07d60c356a1a
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-10-15 12:43:02 -0300

    feat: Criando projeto spring boot

---
+++ server/FitLink/src/main/java/com/puc/fitlink/FitLinkApplication.java
@@ -0,0 +1,13 @@
+package com.puc.fitlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitLinkApplication.class, args);
+    }
+
+}
---
+++ server/FitLink/src/test/java/com/puc/fitlink/FitLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puc.fitlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
