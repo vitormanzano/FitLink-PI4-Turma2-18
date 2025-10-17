e3ae5f0a0cc0c605a98fa4d460deb3ba9d70c010
Guilherme Giusepe Piovezan <161312276+gpiovezan@users.noreply.github.com>
2025-10-17 09:45:26 -0300

    feat: Creating FirstTimeScreen

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.guilherme.myapplication"
+    namespace = "br.edu.puc.fitlink"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.guilherme.myapplication"
+        applicationId = "br.edu.puc.fitlink"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
@@ -56,4 +56,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Navegação
+    implementation ("androidx.navigation:navigation-compose:2.7.7")
+
 }
--- mobile/app/src/main/java/br/edu/puc/fitlink/MainActivity.kt
+++ mobile/app/src/main/java/br/edu/puc/fitlink/MainActivity.kt
@@ -1,47 +1,39 @@
 package br.edu.puc.fitlink
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import br.edu.puc.fitlink.ui.theme.MyApplicationTheme
+import android.provider.Settings
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import br.edu.puc.fitlink.ui.screens.FirstTimeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            val PREFS_NAME = "MyPrefsFile"
+            val settings = getSharedPreferences(PREFS_NAME, 0)
+
+                val isFirstTime = settings.getBoolean("my_first_time", true)
+                if (isFirstTime) {
+                    settings.edit().putBoolean("my_first_time", false).apply()
+                }
+
+                val startDestination = if (isFirstTime) "firstTime" else "signup"
+                val androidId =
+                    Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = startDestination
+                ) {
+                    composable("firstTime") { FirstTimeScreen(navController) }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
-}
