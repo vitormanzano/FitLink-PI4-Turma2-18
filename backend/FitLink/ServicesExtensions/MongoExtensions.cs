29f99636a8b2c8169ace2eca85854fe95975c57b
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-10-25 16:57:34 -0300

    feat: Adicionando conexão com mongo na solution

---
+++ backend/FitLink/ServicesExtensions/MongoExtensions.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Driver;
+
+namespace FitLink.ServicesExtensions
+{
+    public static class MongoExtensions
+    {
+        public static IServiceCollection AddMongo(this IServiceCollection services, IConfiguration configuration)
+        {
+            var mongoConnectionString = configuration.GetConnectionString("MongoConnection");
+
+            var clientSettings = MongoClientSettings.FromConnectionString(mongoConnectionString);
+            var mongoClient = new MongoClient(clientSettings);
+            services.AddSingleton<IMongoClient>(_ => mongoClient);
+
+            services.AddSingleton(sp =>
+            {
+                var mongoClient = sp.GetService<IMongoClient>();
+                var db = mongoClient!.GetDatabase("Cluster-FitLink");
+                return db;
+            });
+            return services;
+        }
+    }
+}
