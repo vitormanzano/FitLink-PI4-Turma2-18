520a3775a0b0665f3622310086fd342db9e819b7
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-10-26 14:59:03 -0300

    feat: Adicionando classe PersonalTrainer e Exceptions

---
+++ backend/FitLink/Exceptions/UserAlreadyExist.cs
@@ -0,0 +1,8 @@
+﻿namespace FitLink.Exceptions
+{
+    public class UserAlreadyExist : Exception
+    {
+        public UserAlreadyExist() : base("Usuário já existe!") { }
+        
+    }
+}
---
+++ backend/FitLink/Models/PersonalTrainer.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace FitLink.Models
+{
+    public class PersonalTrainer
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.String)] // Salva o Guid como string
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+        public string Cref { get; private set; }
+        public string Phone { get; private set; }
+        public string HashedPassword { get; private set; }
+        public bool IsPersonalTrainer { get; private set; } = true;
+
+        public PersonalTrainer(string name, string email, string cpf, string cref, string phone, string hashedPassword)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Email = email;
+            Cpf = cpf;
+            Cref = cref;
+            Phone = phone;
+            HashedPassword = hashedPassword;
+        }
+    }
+}
