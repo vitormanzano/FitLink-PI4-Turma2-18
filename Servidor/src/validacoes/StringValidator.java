00d32e0e1858adc2c2548f87cddb71cd8e9f888d
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-11-18 19:09:30 -0300

    fix: Arrumando string validator pois estava errado a lógica de verificar se é maior, e a de menor. Arrumando os métodos das outras classes

--- Servidor/src/validacoes/StringValidator.java
+++ Servidor/src/validacoes/StringValidator.java
@@ -2,16 +2,20 @@ package validacoes;
 
 // Caso fique mais fácil para do lado do android, pode colocar para retornar uma exception com a mensagem
 public class StringValidator {
+    public static boolean validateIfEmptyOrNull(String value) {
+        return value == null || value.isEmpty();
+    }
+
     public static boolean validateMinimunMaximum(String value, int min, int max) {
         return value.length() > min && value.length() < max;
     }
 
     public static boolean validateIfLessThan(String value, int min) {
-        return value.length() > min;
+        return value.length() < min;
     }
 
     public static boolean validateIfGreaterThan(String value, int max) {
-        return value.length() < max;
+        return value.length() > max;
     }
 
     public static boolean validateIfLengthEquals(String value, int length) {
